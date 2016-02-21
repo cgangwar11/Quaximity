3c6cd55bab47df1709ff3dc5b53c9a53917c637f
cgangwar11 <chandan.gangwar0411@gmail.com>
2016-02-21 19:04:47 +0530

    Update supernode.ino

--- supernode/supernode.ino
+++ supernode/supernode.ino
@@ -1,40 +1,23 @@
-/*
- Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- */
-
-/**
- * Simplest possible example of using RF24Network,
- *
- * RECEIVER NODE
- * Listens for messages from the transmitter and prints them out.
- */
- 
  int ledPin=4;
 
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
 
-// nRF24L01(+) radio attached using Getting Started board 
+
 RF24 radio(9,10);
 
-// Network uses that radio
+
 RF24Network network(radio);
 
-// Address of our node
+
 const uint16_t this_node = 5;
 
-// Address of the other node
 const uint16_t other_node = 4;
 
-//Set threshold for SW420
 int threshold=100;
 
-// Structure of our payload
 struct payload_rt
 {
   long record1;
@@ -51,20 +34,18 @@ void setup(void)
  
   SPI.begin();
   radio.begin();
-  network.begin(/*channel*/ 90, /*node address*/ this_node);
+  network.begin( 90, this_node);
 }
 
 void loop()
 {
-  // Pump the network regularly
-  network.update();
-  //Serial.print("Hello");
 
-  // Is there anything ready for us?
+  network.update();
+
   while (network.available())
   {
     Serial.println("Available he bhaaaai");
-    // If so, grab it and print it out
+   
     RF24NetworkHeader header;
     payload_rt payload;
     network.read(header,&payload,sizeof(payload));
@@ -78,11 +59,11 @@ void loop()
     if(payload.record1>threshold )
     {
       digitalWrite(ledPin,HIGH);
-   //   Serial.println("High intensity recorded!!!!!!!!!");
+  
       delay(1000);
       digitalWrite(ledPin,LOW);
     }
   }
 }
-/* vim:ai:cin:sts=2 sw=2 ft=cpp*/
+
 
